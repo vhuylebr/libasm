46c64fe288f9446a6ce11236d04dda97a1e7bc50
valerian <valerian@localhost.localdomain>
2018-03-08 05:27:53 +0100

    memmove is okkkkkk

--- memmove.asm
+++ memmove.asm
@@ -4,15 +4,52 @@ SECTION .text:
 memmove:
 	xor rax, rax
     xor r9b, r9b
+	cmp rdi, rsi
+	jg _is_loop_2
+	jmp _loop
+
+_second:
+	dec rdx
+	jmp _loop
 
 _loop:
-	cmp rax, rdx
+	cmp rdx, rax
 	je _end
     mov r9b, [rsi + rax]
     mov [rdi + rax], r9b
 	inc rax
 	jmp _loop
 
+_is_loop_2:
+	call strlen
+	cmp rdi, [rsi + rax]
+	jg _loop_2
+	xor rax, rax
+	jmp _loop
+
+_loop_2:
+	cmp rdx, -1
+	jz _end
+	cmp BYTE [rdi + rdx], 0
+	jz _second
+	cmp BYTE [rsi + rdx], 0
+	jz _second
+    mov r9b, [rsi + rdx]
+    mov [rdi + rdx], r9b
+	dec rdx
+	jmp _loop_2
+
 _end:
-    mov rax, rdi
+	mov rax, rdi
+	ret
+
+strlen:
+	xor rax, rax
+_loop_strlen:
+	cmp BYTE [rdi + rax], 0
+	jz _end_strlen
+	inc rax
+	jmp _loop_strlen
+
+_end_strlen:
 	ret
